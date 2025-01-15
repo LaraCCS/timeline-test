9cfa15fa1f4111273131008e5e37c9ec2fface61
LaraCCS <37679017+LaraCCS@users.noreply.github.com>
2025-01-15 14:11:33 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const timeline_config = {
+  source: 'https://docs.google.com/spreadsheets/d/1d5DceZh8VxBm8xHbxuHq9kzST94mLP5Eq_xjDltJKyE/pubhtml',
+  height: '600',
+};
+
+new TL.Timeline('timeline-embed', timeline_config);
